633fad1c25c09fa89559136f0f5ca23894a703ff
Vladyslav Prokhnitskyi <v.prokhnitskyi@Vladyslavs-MacBook-Pro.local>
2023-11-16 18:49:42 +0200

    Initial commit

---
+++ src/main/java/Division.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Division {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the first any integer: ");
+		int dividend = Integer.parseInt(scanner.nextLine());
+		System.out.println("Enter the second any integer: ");
+		int divider = Integer.parseInt(scanner.nextLine());
+		System.out.println(LogicDivision.makeDivision(dividend, divider));
+
+	}
+}
